f5d218be257e74a426772159e3c54c8e1705fa8b
Nic Caine <ncaine@gmail.com>
2017-01-06 16:41:08 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var app = express()
+
+var middleware = {
+	requireAutentication: function(req, res, next) {
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + req.originalUrl);
+		next();
+	}
+};
+
+// app.use(middleware.requireAutentication);
+app.use(middleware.logger);
+
+
+app.get('/about', middleware.requireAutentication, function(req, res) {
+	res.send('about us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var PORT = 3000;
+app.listen(PORT, function(){
+	console.log('express server started on port: ' + PORT);
+});
+
